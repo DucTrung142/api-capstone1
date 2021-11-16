3b737c9c731ab068f69e076d2193d219fffac74d
DucTrung142 <doductrung142@gmail.com>
2021-11-16 21:51:33 +0700

    test upload

--- src/config/db/multer.js
+++ src/config/db/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const fileFilter = (req, file, cb) => {
   const extensionOfPath = path.extname(file.originalname);
-  if (!['.jpg', '.png'].includes(extensionOfPath)) {
+  if (!['.jpg', '.png', '.docx'].includes(extensionOfPath)) {
     cb(new Error('File type is not supported'));
   }
 
--- src/routes/api/upload.js
+++ src/routes/api/upload.js
@@ -9,7 +9,7 @@ router.post('/', upload.single('image'), async (req, res) => {
     const result = await cloudinary.uploader.upload(req.file.path, {
       folder: 'image-uploader',
       flags: 'attachment:pretty_flower',
-      width: 160,
+      // width: 160,
     });
     res.status(200).json({
       name: req.file.originalname,
