122c01045ba2a99847ea0eade94ef6f9105d1210
DucTrung142 <doductrung142@gmail.com>
2021-11-16 21:56:50 +0700

    fix upload image

--- src/config/db/multer.js
+++ src/config/db/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const fileFilter = (req, file, cb) => {
   const extensionOfPath = path.extname(file.originalname);
-  if (!['.jpg', '.png', '.pdf'].includes(extensionOfPath)) {
+  if (!['.jpg', '.png'].includes(extensionOfPath)) {
     cb(new Error('File type is not supported'));
   }
 
