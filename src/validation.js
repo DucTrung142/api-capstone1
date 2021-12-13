79b247c938521ae8fbbbd45484eccb603f6830af
DucTrung142 <doductrung142@gmail.com>
2021-12-13 21:22:52 +0700

    test

--- src/validation.js
+++ src/validation.js
@@ -1,35 +1,32 @@
-import { object, string, date, required, number } from 'joi';
+const Joi = require('joi');
 
-const registerValidation = object({
-  fullname: string().min(6).max(30).required(),
-  username: string().min(6).max(30).lowercase().required(),
-  password: string().min(6).max(30).required(),
-  phone: string().min(9).max(11).pattern(new RegExp('^[0-9]')),
-  birthday: date().min('1-1-1800').max('12-31-2021'),
-  user_type: string().uppercase(),
+const registerValidation = Joi.object({
+  fullname: Joi.string().min(6).max(30).required(),
+  username: Joi.string().min(6).max(30).lowercase().required(),
+  password: Joi.string().min(6).max(30).required(),
+  phone: Joi.string().min(9).max(11).pattern(new RegExp('^[0-9]')),
+  birthday: Joi.date().min('1-1-1800').max('12-31-2021'),
+  user_type: Joi.string().uppercase(),
 });
-const loginValidation = object({
-  username: string().min(6).max(30).required(),
-  password: string().min(6).max(30).required(),
+const loginValidation = Joi.object({
+  username: Joi.string().min(6).max(30).required(),
+  password: Joi.string().min(6).max(30).required(),
 });
-const questionValidation = object({
-  id_exam: required(),
-  exam_topic_db: required(),
-  hourOpenDb: required(),
-  minuteOpenDb: required(),
-  secondOpenDb: required(),
-  hourDueDb: required(),
-  minuteDueDb: required(),
-  secondDueDb: required(),
-  totalScoreDb: required(),
-  totalQuestionDb: number(),
-  quiz: required(),
-  results: required(),
+const questionValidation = Joi.object({
+  id_exam: Joi.required(),
+  exam_topic_db: Joi.required(),
+  hourOpenDb: Joi.required(),
+  minuteOpenDb: Joi.required(),
+  secondOpenDb: Joi.required(),
+  hourDueDb: Joi.required(),
+  minuteDueDb: Joi.required(),
+  secondDueDb: Joi.required(),
+  totalScoreDb: Joi.required(),
+  totalQuestionDb: Joi.number(),
+  quiz: Joi.required(),
+  results: Joi.required(),
 });
 
-const _registerValidation = registerValidation;
-export { _registerValidation as registerValidation };
-const _loginValidation = loginValidation;
-export { _loginValidation as loginValidation };
-const _questionValidation = questionValidation;
-export { _questionValidation as questionValidation };
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
+module.exports.questionValidation = questionValidation;
