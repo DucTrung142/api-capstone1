8f7b171107a86b4f58ffbe6d31c4cc8f6eb098e3
DucTrung142 <doductrung142@gmail.com>
2021-12-19 02:49:09 +0700

    fix validation

--- src/validation.js
+++ src/validation.js
@@ -2,17 +2,17 @@ const Joi = require('joi');
 
 const registerValidation = Joi.object({
   fullname: Joi.string().min(6).max(30).required().messages({
-    'string.empty': ` You need to enter the fullname`,
+    'string.empty': `Need to enter enough information`,
     'string.max': `Fullname Characters whose length exceeds 30`,
     'string.min': `Fullname characters less than 6`,
   }),
   username: Joi.string().min(6).max(30).required().messages({
-    'string.empty': ` You need to enter the username`,
+    'string.empty': `Need to enter enough information`,
     'string.max': `Username Characters whose length exceeds 30`,
     'string.min': `Username characters less than 6`,
   }),
   password: Joi.string().min(6).max(30).required().messages({
-    'string.empty': ` You need to enter the password`,
+    'string.empty': `Need to enter enough information`,
     'string.max': `Password characters whose length exceeds 30`,
     'string.min': `Password characters less than 6`,
   }),
@@ -22,56 +22,55 @@ const registerValidation = Joi.object({
     .max(11)
     .pattern(new RegExp('^[0-9]'))
     .messages({
-      'string.empty': ` You need to enter the phone`,
+      'string.empty': `Need to enter enough information`,
       'string.max': `Phone characters whose length exceeds 9`,
       'string.min': `phone characters less than 11`,
     }),
   birthday: Joi.date().min('1-1-1800').max('12-31-2021'),
   user_type: Joi.string().required().messages({
-    'string.empty': ` You need to enter the user_type`,
+    'string.empty': `Need to enter enough information`,
   }),
 });
 const loginValidation = Joi.object({
   username: Joi.string().min(6).max(30).required().messages({
-    'string.empty': ` You need to enter the username`,
+    'string.empty': `Need to enter enough information`,
     'string.max': `Username Characters whose length exceeds 30`,
     'string.min': `Username characters less than 6`,
   }),
   password: Joi.string().min(6).max(30).required().messages({
-    'string.empty': ` You need to enter the password`,
+    'string.empty': `Need to enter enough information`,
     'string.max': `Password characters whose length exceeds 30`,
     'string.min': `Password characters less than 6`,
   }),
 });
 const questionValidation = Joi.object({
   id_exam: Joi.string().required().messages({
-    'string.empty': ` You need to enter id exam`,
+    'string.empty': `Need to enter enough information`,
   }),
   exam_date_db: Joi.date().min('now').required().messages({
     'date.min': `Invalid exam date`,
-    'any.empty': ` You need to enter the exam date`,
-    'date.base': `You need to enter the correct date format`,
+    'date.base': `re-enter the date`,
   }),
   exam_topic_db: Joi.string().required().messages({
-    'string.empty': ` You need to enter the exam topics`,
+    'string.empty': `Need to enter enough information`,
   }),
   hourOpenDb: Joi.string().required().messages({
-    'string.empty': ` You need to enter the exam start hours`,
+    'string.empty': `Need to enter enough information`,
   }),
   minuteOpenDb: Joi.string().required().messages({
-    'string.empty': ` You need to enter the exam start minute`,
+    'string.empty': `Need to enter enough information`,
   }),
   secondOpenDb: Joi.string().required().messages({
-    'string.empty': ` You need to enter the exam start second`,
+    'string.empty': `Need to enter enough information`,
   }),
   hourDueDb: Joi.string().required().messages({
-    'string.empty': ` You need to enter the exam due hours`,
+    'string.empty': `Need to enter enough information`,
   }),
   minuteDueDb: Joi.string().required().messages({
-    'string.empty': ` You need to enter the exam due minute`,
+    'string.empty': `Need to enter enough information`,
   }),
   secondDueDb: Joi.string().required().messages({
-    'string.empty': ` You need to enter the exam due second`,
+    'string.empty': `Need to enter enough information`,
   }),
   totalScoreDb: Joi.number().required().messages({
     'number.base': `You need to enter the correct number format total score`,
@@ -81,14 +80,14 @@ const questionValidation = Joi.object({
     .items({
       name_question: Joi.number(),
       question_type: Joi.string().required().messages({
-        'string.empty': ` You need to enter the question type`,
+        'string.empty': ` Need to enter enough information`,
       }),
       question_content: Joi.string().max(700).required().messages({
-        'string.empty': ` You need to enter the question content`,
+        'string.empty': `Need to enter enough information`,
         'string.max': `Characters whose length exceeds 700`,
       }),
       point_question: Joi.number().positive().required().messages({
-        'number.empty': ` You need to enter the point question`,
+        'number.empty': ` Need to enter enough information`,
         'number.positive': `Question score must be greater than 0`,
         'number.base': `You need to enter the correct number format point question`,
       }),
